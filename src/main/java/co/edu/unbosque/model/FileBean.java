9e6815d621e7b180846099bb62c5e921079d7125
R3C0NF1G <juliandavidguzman21@gmail.com>
2023-09-29 17:14:09 -0500

    algoritmo kmp implementado, subrayar 50%

--- src/main/java/co/edu/unbosque/model/FileBean.java
+++ src/main/java/co/edu/unbosque/model/FileBean.java
@@ -34,7 +34,86 @@ public class FileBean {
 		// Ejemplo simple que subraya todas las coincidencias en el texto:
 		// texto = texto.replaceAll(palabra, "<span style='text-decoration:
 		// underline;'>" + palabra + "</span>");
+		
+		textToSearch = textToSearch.toLowerCase();
+		content = content.toLowerCase();
+		
+		int M = textToSearch.length();
+        int N = content.length();
+ 
+        // create lps[] that will hold the longest
+        // prefix suffix values for pattern
+        int lps[] = new int[M];
+        int j = 0; // index for pat[]
+ 
+        // Preprocess the pattern (calculate lps[]
+        // array)
+        computeLPSArray(textToSearch, M, lps);
+ 
+        int i = 0; // index for txt[]
+        while (i < N) {
+            if (textToSearch.charAt(j) == content.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                System.out.println("Found pattern "
+                                   + "at index " + (i - j));
+                
+                j = lps[j - 1];
+                
+                String txtFound = content.substring(i - j, i - j + textToSearch.length());
+                
+                content = content.replaceAll(txtFound, "<span style='background-color:yellow;'>" + txtFound + "</span>");
+                
+                break;
+            }
+ 
+            // mismatch after j matches
+            else if (i < N && textToSearch.charAt(j) != content.charAt(i)) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
 	}
+	
+	 void computeLPSArray(String pat, int M, int lps[])
+	    {
+	        // length of the previous longest prefix suffix
+	        int len = 0;
+	        int i = 1;
+	        lps[0] = 0; // lps[0] is always 0
+	 
+	        // the loop calculates lps[i] for i = 1 to M-1
+	        while (i < M) {
+	            if (pat.charAt(i) == pat.charAt(len)) {
+	                len++;
+	                lps[i] = len;
+	                i++;
+	            }
+	            else // (pat[i] != pat[len])
+	            {
+	                // This is tricky. Consider the example.
+	                // AAACAAAA and i = 7. The idea is similar
+	                // to search step.
+	                if (len != 0) {
+	                    len = lps[len - 1];
+	 
+	                    // Also, note that we do not increment
+	                    // i here
+	                }
+	                else // if (len == 0)
+	                {
+	                    lps[i] = len;
+	                    i++;
+	                }
+	            }
+	        }
+	    }
 
 	public void upload() {
 		if (file != null) {
